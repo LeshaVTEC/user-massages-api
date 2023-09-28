fcb4a00832b2ed1411bc3d7a7ca2bd28970a59a1
Anton Makatrau <74259724+AntonMakatrov@users.noreply.github.com>
2023-09-28 16:39:10 +0300

    Task3/messages api (#2)
    * entity Message created
    
    * Message dao, dto created
    
    * Message dao, dto created
    
    * Fix name
    
    * Message services created
    
    * MessageServlet created
    
    * added JSP pages message.jsp and chats.jsp
    
    * pom.xml editedw
    
    * UserNotFoundException created, added try/catch in MessageServlet
    
    * jsp fixed
    
    * AdminSecurityFilter and UserSecurityFilter added
    
    ---------
    
    Co-authored-by: Anton Makatrau <antonmakatrau@gmail.com>

--- src/main/java/by/it_academy/team1/usermassages/core/dto/MassageDto.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.team1.usermassages.core.dto;
-
-import java.time.LocalDateTime;
-
-public class MassageDto {
-    private String usernameTo;
-    private String text;
-
-}
--- src/main/java/by/it_academy/team1/usermassages/service/api/IMessageService.java
+++ src/main/java/by/it_academy/team1/usermassages/service/api/IMessageService.java
@@ -1,8 +1,16 @@
 package by.it_academy.team1.usermassages.service.api;
 
+import by.it_academy.team1.usermassages.core.dto.MessageDto;
+import by.it_academy.team1.usermassages.core.entity.Message;
+import by.it_academy.team1.usermassages.core.entity.User;
+
+import java.util.List;
+
 /**
  * @author makatrov_anton@mail.ru
  */
-public interface IMessageService {
+public interface IMessageService extends ICRUDService<Message>{
+    List<Message> getMessagesOfUser(String username);
 
+    void sendMessage(MessageDto message);
 }
