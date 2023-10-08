7a4eb1b652b084e95b8b18bc0b6feeee17ea0432
alexey <ivaldm90@gmail.com>
2023-10-08 20:31:32 +0300

    Revision statistics number 2, add StatisticsService and statistics api

--- src/main/java/by/it_academy/team1/usermessages/listeners/ActiveSessionUsersListener.java
+++ src/main/java/by/it_academy/team1/usermessages/listeners/ActiveSessionUsersListener.java
@@ -1,32 +1,28 @@
 package by.it_academy.team1.usermessages.listeners;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
+import by.it_academy.team1.usermessages.service.StatisticsService;
+import by.it_academy.team1.usermessages.service.api.IStatisticsService;
+import jakarta.servlet.http.HttpSessionAttributeListener;
+import jakarta.servlet.http.HttpSessionBindingEvent;
 
 public class ActiveSessionUsersListener implements HttpSessionAttributeListener {
+    private final IStatisticsService statisticsService = StatisticsService.getInstance();
 
-    public static final String TOTAL_USERS_SESSION_ATTRIBUTE_KEY = "total_users_session_attribute";
 
     @Override
     public void attributeAdded(HttpSessionBindingEvent event) {
-        ServletContext context = event.getSession().getServletContext();
-        Object contextValue = context.getAttribute(TOTAL_USERS_SESSION_ATTRIBUTE_KEY);
-        if (contextValue == null) {
-            context.setAttribute(TOTAL_USERS_SESSION_ATTRIBUTE_KEY, 1);
-        } else {
-            context.setAttribute(
-                    TOTAL_USERS_SESSION_ATTRIBUTE_KEY,
-                    Integer.parseInt(contextValue.toString()) + 1
-            );
-        }
+//        if("user".equalsIgnoreCase(event.getName()) && event.getValue() != null){
+        var servletContext = event.getSession().getServletContext();
+        servletContext.log("attributeAdded: " + event.getName() + event.getValue());
+            this.statisticsService.incSessionCount();
+//        }
     }
 
     @Override
     public void attributeRemoved(HttpSessionBindingEvent event) {
-        ServletContext context = event.getSession().getServletContext();
-        Object contextValue = context.getAttribute(TOTAL_USERS_SESSION_ATTRIBUTE_KEY);
-        context.setAttribute(TOTAL_USERS_SESSION_ATTRIBUTE_KEY, Integer.parseInt(contextValue.toString()) - 1);
+        if("user".equalsIgnoreCase(event.getName())){
+            this.statisticsService.decSessionCount();
+        }
     }
 
     @Override
